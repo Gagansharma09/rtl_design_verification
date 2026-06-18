9597faecabe4fa6e414dbf5e164004f271a4375f
gagan <163728342+Gagansharma09@users.noreply.github.com>
2026-06-18 08:06:46 +0530

    Update half_adder.sv

--- arithmetic/half_adder/half_adder.sv
+++ arithmetic/half_adder/half_adder.sv
@@ -1,9 +1,10 @@
-// HDLBits: Half Adder
-// https://hdlbits.01xz.net/wiki/hadd
 module half_adder(
-    input  logic a, b,
-    output logic sum, cout
-);
-    assign sum  = a ^ b;
-    assign cout = a & b;
-endmodule
+     input logic a,
+     input logic b ,
+    output logic cout,sum);
+     
+    
+      assign  sum = a ^ b;
+      assign  cout = a & b;
+    
+endmodule 
