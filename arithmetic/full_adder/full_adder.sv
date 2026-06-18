b70b668f5fd7482410c4313b2ed943ed173bddc3
gagan <163728342+Gagansharma09@users.noreply.github.com>
2026-06-18 10:46:43 +0530

    Update full_adder.sv

--- arithmetic/full_adder/full_adder.sv
+++ arithmetic/full_adder/full_adder.sv
@@ -1,9 +1,13 @@
-// HDLBits: Full Adder
-// https://hdlbits.01xz.net/wiki/fadd
+// Code your design here
 module full_adder(
-    input  logic a, b, cin,
-    output logic sum, cout
-);
-    assign sum  = a ^ b ^ cin;
-    assign cout = (a & b) | (b & cin) | (a & cin);
-endmodule
+      input logic in1,
+      input logic in2,
+      input logic cin,
+      output logic cout,
+  output logic sum);
+      
+//assignement
+  assign sum = in1 ^in2 ^cin;
+  assign cout = (in1&in2)|(in2&cin)|(cin&in1);
+     
+endmodule 
